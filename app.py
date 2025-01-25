d494e7ce86006e20c070311dc97c7ac12a4aec68
Ali Ahmad <116615877+aliahmad-aa@users.noreply.github.com>
2025-01-25 23:38:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 if uploaded_file is not None:
     # Display the uploaded image
     image = Image.open(uploaded_file).convert("RGB")  # Convert image to RGB
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if model:
         # Predict gemstone
